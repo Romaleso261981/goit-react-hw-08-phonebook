7670cd2b8676d7ff924bee48872ca7be88549d4f
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-26 14:09:52 +0300

    added HomeView

--- src/components/HomeView/HomeView.jsx
+++ src/components/HomeView/HomeView.jsx
@@ -1,79 +1,13 @@
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-// import { nanoid } from 'nanoid';
-import { addContactApi} from '../../redux/operations/operations';
-import { fetchContact} from '../../redux/operations/operations';
-import { getItems } from '../../redux/tasksSlice';
-import Notiflix from 'notiflix';
+import React from 'react';
+import {Container, Title} from './HomeViewStyle'
 
-import {
-  InputLabel,
-  FormWrap,
-  InputSpan,
-  InputSpanName,
-  FormBtn,
-  InputForm,
-} from './HomeViewStyle.js';
 
-const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-  const dispatch = useDispatch();
-  const items = useSelector(getItems);
+const HomeView = () => {
+  <Container>
+    <Title>
+      Головна сторінка нашої программи
+    </Title>
+  </Container>
+}
 
-  useEffect(() => {
-    dispatch(fetchContact());
-  }, [dispatch]);
-
-  const handleChange = event => {
-    const { name, value } = event.target;
-    if (name === 'name') {
-      setName(value);
-    } else if (name === 'number') {
-      setNumber(value);
-    }
-  };
-
-  const handleSubmitForm = event => {
-    if (items.find(item => item.name === name)) {
-      Notiflix.Notify.warning('This contact is already exists');
-      return;
-    }
-    event.preventDefault();
-    dispatch(addContactApi( {name, number} ));
-    setName('');
-    setNumber('');
-  };
-
-  return (
-    <FormWrap onSubmit={handleSubmitForm}>
-      <InputLabel>
-        <InputSpanName>Name</InputSpanName>
-        <InputForm
-          onChange={handleChange}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          value={name}
-        />
-      </InputLabel>
-      <InputLabel>
-        <InputSpan>Number</InputSpan>
-        <InputForm
-          onChange={handleChange}
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          value={number}
-        />
-      </InputLabel>
-      <FormBtn type="submit">Add contact</FormBtn>
-    </FormWrap>
-  );
-};
-
-export default ContactForm;
+export default HomeView;
