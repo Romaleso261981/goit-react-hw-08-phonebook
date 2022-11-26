b37127435e3fb4c7764f92928f87def67e5a6ead
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-26 14:41:20 +0300

    it is work

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,63 @@
-import {Route} from 'react-router-dom'
+// import { useEffect, lazy } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Route, Routes } from 'react-router-dom';
+// import { PrivateRoute } from './PrivateRoute';
+// import { RestrictedRoute } from './RestrictedRoute';
+// import { refreshUser } from 'redux/auth/operations';
+// import { useAuth } from 'hooks';
+// import { Layout } from './Layout';
 import AppBar from './components/HomeView/HomeView';
-import { RegisterView } from '';
-import { HomeView } from '';
-import { ContactListView } from './components/ContactListView/ContactListView';
-import { LoginView } from './components/ContactListView/ContactListView';
-import { Container } from './components/AppStyle';
+// import { Container } from './components/AppStyle';
+
+// const HomeView = lazy(() => import(''));
+// const RegisterView = lazy(() => import(''));
+// const LoginView = lazy(() => import(''));
+// const ContactListView = lazy(() => import(''));
 
 export default function App() {
-  return (
-    <Container>
-    <AppBar />
-    <switch>
-      < Route exact path="/" component={HomeView} />
-      < Route path="/register" component={RegisterView} />
-      < Route path="/register" component={LoginView} />
-      < Route path="/register" component={ContactListView} />
-    </switch>
 
-  </Container>
-  )
+  // const dispatch = useDispatch();
+  // const { isRefreshing } = useAuth();
+
+  // useEffect(() => {
+  //   dispatch(refreshUser());
+  // }, [dispatch]);
+
+
+  return true ? (
+    <b>Refreshing user...</b>
+  ) : (
+      <>
+        <AppBar/>
+        {/* <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomeView />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<RegisterView />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<LoginView />} />
+          }
+        />
+        <Route
+          path="/tasks"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactListView />} />
+          }
+        />
+      </Route>
+    </Routes> */}
+      </>
+    
+  );
   
 }
+
+
+
+
