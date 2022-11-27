871d6ee54e119c72c97d720118052379a53622c0
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-27 09:59:34 +0300

    added LoginView

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import AppBar from './components/HomeView/HomeView';
-import AppBar from './components/HomeView/HomeView';
+import FormPage from './components/RegisterView/RegisterView';
 import AppBar from './components/HomeView/HomeView';
 import AppBar from './components/HomeView/HomeView';
 
