07334fd3904f3302f56d8ae2d4a089642c04b5b3
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-11-28 09:47:07 +0300

    added AuthNav

---
+++ src/components/AuthNav/AuthNav.styled.js
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+export const Link = styled(NavLink)`
+   display: inline-block;
+    text-decoration: none;
+    padding: 12px;
+    font-weight: 700;
+    color: #2a363b;
+`;
+
+// .link.active {
+//     color: #e84a5f;
+//   }
+
+  
+  
+  
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from 'App';
-import { store, persistor } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
 import './index.css';
 
 const basename =
