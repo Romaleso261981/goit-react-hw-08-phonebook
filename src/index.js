78552c23388b9be237c532b663bb565e4c0fb73c
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-12-04 12:06:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
               Width: '100wv',
               Height: '500px',
               backgroundImage: 'url(https://source.unsplash.com/random)',
+              backgroundSize: 'cover',
             }}
           >
             <App />
